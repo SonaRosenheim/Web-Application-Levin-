a3154b0994fdfad3fc4080536d5fc875ed0dc2f6
SonaRosenheim <skyshadow723@gmail.com>
2024-10-08 21:02:07 +0800

    *commit*

---
+++ DelaCruzJr/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DelaCruzJr project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DelaCruzJr.settings')
+
+application = get_wsgi_application()
