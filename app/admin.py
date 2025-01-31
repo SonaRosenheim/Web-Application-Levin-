1cd105aa7e839087c081c35e134b2e47c2dd4d0c
SonaRosenheim <132370947+SonaRosenheim@users.noreply.github.com>
2025-01-31 16:06:37 +0800

    Delete app directory

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, UserAddress, UserPayment, Product, ProductInventory, Category
-
-admin.site.register(User)
-admin.site.register(UserAddress)
-admin.site.register(UserPayment)
-admin.site.register(Product)
-admin.site.register(ProductInventory)
-admin.site.register(Category)
